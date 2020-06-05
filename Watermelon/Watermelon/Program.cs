6edc2511f94e992bcb66078f9ee8afea0b8eb8b8
raihannishat.swe@gmail.com <en1152nasir>
2020-06-05 18:12:21 +0600

    Watermelon

---
+++ Watermelon/Watermelon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+
+            if (Weight(w))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+        static bool Weight(int weight)
+        {
+            if (weight % 2 == 0 && weight > 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
