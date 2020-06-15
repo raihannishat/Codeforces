8917f1976bdac627119d72e552d1a344bf9a5112
raihannishat.swe@gmail.com <en1152nasir>
2020-06-15 16:14:13 +0600

    Bit++

---
+++ Bit++/Bit++/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string operation = Console.ReadLine();
+
+                if (Equals(operation, "X++") || Equals(operation, "++X"))
+                {
+                    x++;
+                }
+                else
+                {
+                    x--;
+                }
+            }
+
+            Console.WriteLine($"{x}");
+        }
+    }
+}
