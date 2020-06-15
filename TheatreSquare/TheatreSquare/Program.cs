79dc9c2b1f4f08d216ee9d16659ba3f354051bc2
raihannishat.swe@gmail.com <en1152nasir>
2020-06-15 14:47:39 +0600

    Theatre Square

---
+++ TheatreSquare/TheatreSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TheatreSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            long m = long.Parse(line[0]);
+            long n = long.Parse(line[1]);
+            long a = long.Parse(line[2]);
+
+            long res = ((m + a - 1) / a) * ((n + a - 1) / a);
+
+            Console.WriteLine("{0}", res);   
+        }
+    }
+}
