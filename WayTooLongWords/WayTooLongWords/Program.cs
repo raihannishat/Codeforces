9e434f6a10bb45cd76a493f596274799fe70475d
raihannishat.swe@gmail.com <en1152nasir>
2020-06-15 16:13:52 +0600

    WayTooLongWords

---
+++ WayTooLongWords/WayTooLongWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WayTooLongWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string word = Console.ReadLine();
+
+                if (word.Length <= 10)
+                {
+                    Console.WriteLine(word);
+                }
+                else
+                {
+                    Console.WriteLine($"{word[0]}{word.Length - 2}{word[word.Length - 1]}");
+                }
+            }
+        }
+    }
+}
